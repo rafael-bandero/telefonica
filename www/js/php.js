9b1cb507b7f4c23e1a7dd7cc52cda1773abe3427
Rafael Banderó <rafaelbandero@gmail.com>
2024-09-05 15:02:02 -0300

    Added php.js

---
+++ www/js/php.js
@@ -0,0 +1,5 @@
+const apiUrl = 'https://localhost/request-handler.php';
+const fetchUrl = `${apiUrl}?action=fetchAll`;
+const deleteUrl = `${apiUrl}?action=delete&id=`;
+const createUrl = `${apiUrl}?action=save`;
+const saveUrl = `${createUrl}&id=`;
