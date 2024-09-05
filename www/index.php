35b0d2f38d35df146102bd0fcfddaae348f92c35
Rafael Banderó <rafaelbandero@gmail.com>
2024-09-05 11:20:30 -0300

    Included basic index.php to start PHP developmnet

---
+++ www/index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>To-Do List</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="css/custom.css" rel="stylesheet">
+</head>
+
+<body>
+    <header class="telefonica">
+        <nav class="navbar">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img src="images/logo.png" width="34" height="34" title="Movistar Logo">
+                </a>
+
+                <div class="btn-group" role="group">
+                    <button type="button" class="btn nav-link p-2" title="Search"><i class="bi bi-search"></i></button>
+                    <button type="button" class="btn nav-link p-2" title="Configs"><i class="bi bi-sliders"></i></button>
+                    <button type="button" class="btn nav-link p-2" title="Close"><i class="bi bi-x-lg"></i></button>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <div class="container pt-4">
+        <h6 class="text-uppercase">Project</h6>
+
+        <div class="row">
+            <!-- content goes here -->
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
